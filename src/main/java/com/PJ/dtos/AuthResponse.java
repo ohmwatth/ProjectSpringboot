0e27f0cc777b175b42b2d2e3bc5fe4a5b1c0663c
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-10-07 00:33:36 +0700

    Update

--- src/main/java/com/PJ/dtos/AuthResponse.java
+++ src/main/java/com/PJ/dtos/AuthResponse.java
@@ -1,19 +1,11 @@
 package com.PJ.dtos;
 
 import lombok.*;
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String token;
-
-	public AuthResponse(String token) {
-		super();
-		this.token = token;
-	}
-
-	public AuthResponse() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-    
-    
 }
